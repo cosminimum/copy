f5dfcc16782ecba6e357e53ad3ff282fc7045b41
Cosmin Gheorghila <cosmin@gheorghila.com>
2025-11-21 01:19:59 +0200

    Final fix for withdraw

--- lib/contracts/withdrawal-module-abi.ts
+++ lib/contracts/withdrawal-module-abi.ts
@@ -9,24 +9,83 @@
 export const USER_WITHDRAWAL_MODULE = '0x9C4d41503aCbF6433f7D73a1507FDF8845Be4e49' as const
 export const USDC_E_ADDRESS = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174' as const
 
-// UserWithdrawalModule ABI
+// UserWithdrawalModule ABI (JSON format for wagmi/viem)
 export const USER_WITHDRAWAL_MODULE_ABI = [
-  // View functions
-  'function isAuthorized(address safe, address user) external view returns (bool)',
-  'function getAuthorizedUser(address safe) external view returns (address)',
-
-  // Withdrawal functions (user-callable)
-  'function withdrawToken(address safe, address token, uint256 amount) external',
-  'function withdrawAllTokens(address safe, address token) external',
-
-  // Admin functions (operator-only, for setup)
-  'function authorizeUser(address user) external',
-  'function revokeUser() external',
+  {
+    name: 'isAuthorized',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [
+      { name: 'safe', type: 'address' },
+      { name: 'user', type: 'address' },
+    ],
+    outputs: [{ name: '', type: 'bool' }],
+  },
+  {
+    name: 'getAuthorizedUser',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [{ name: 'safe', type: 'address' }],
+    outputs: [{ name: '', type: 'address' }],
+  },
+  {
+    name: 'withdrawToken',
+    type: 'function',
+    stateMutability: 'nonpayable',
+    inputs: [
+      { name: 'safe', type: 'address' },
+      { name: 'token', type: 'address' },
+      { name: 'amount', type: 'uint256' },
+    ],
+    outputs: [],
+  },
+  {
+    name: 'withdrawAllTokens',
+    type: 'function',
+    stateMutability: 'nonpayable',
+    inputs: [
+      { name: 'safe', type: 'address' },
+      { name: 'token', type: 'address' },
+    ],
+    outputs: [],
+  },
+  {
+    name: 'authorizeUser',
+    type: 'function',
+    stateMutability: 'nonpayable',
+    inputs: [{ name: 'user', type: 'address' }],
+    outputs: [],
+  },
+  {
+    name: 'revokeUser',
+    type: 'function',
+    stateMutability: 'nonpayable',
+    inputs: [],
+    outputs: [],
+  },
 ] as const
 
 // ERC20 ABI (minimal, for balance checks)
 export const ERC20_ABI = [
-  'function balanceOf(address account) external view returns (uint256)',
-  'function decimals() external view returns (uint8)',
-  'function symbol() external view returns (string)',
+  {
+    name: 'balanceOf',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [{ name: 'account', type: 'address' }],
+    outputs: [{ name: '', type: 'uint256' }],
+  },
+  {
+    name: 'decimals',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [],
+    outputs: [{ name: '', type: 'uint8' }],
+  },
+  {
+    name: 'symbol',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [],
+    outputs: [{ name: '', type: 'string' }],
+  },
 ] as const
